6011800eea10f4673f2111d93aa98b98a61fd8c3
Robert Tivy <roberttivy@Laurens-MacBook-Pro-2.local>
2023-01-18 08:11:27 -0800

    Lots of development

---
+++ ko/plots.js
@@ -0,0 +1,124 @@
+const plots = [
+  {
+    id: "p1", plotnum: 1, shedpadtop: 752, shedpadleft: 392,
+    owner: -1, price: 0,
+    holes: [
+    ]
+  },
+  {
+    id: "p2", plotnum: 2, shedpadtop: 752, shedpadleft: 392,
+    owner: -1, price: 0,
+    holes: [
+    ]
+  },
+  {
+    id: "p3", plotnum: 3, shedpadtop: 752, shedpadleft: 392,
+    owner: -1, price: 0,
+    holes: [
+    ]
+  },
+  {
+    id: "p4", plotnum: 4, shedpadtop: 752, shedpadleft: 392,
+    owner: -1, price: 0,
+    holes: [
+    ]
+  },
+  {
+    id: "p5", plotnum: 5, shedpadtop: 752, shedpadleft: 392,
+    owner: -1, price: 0,
+    holes: [
+    ]
+  },
+  {
+    id: "p6", plotnum: 6, shedpadtop: 752, shedpadleft: 392,
+    owner: -1, price: 0,
+    holes: [
+    ]
+  },
+  {
+    id: "p7", plotnum: 7, shedpadtop: 752, shedpadleft: 392,
+    owner: -1, price: 0,
+    holes: [
+    ]
+  },
+  {
+    id: "p8", plotnum: 8, shedpadtop: 752, shedpadleft: 392,
+    owner: -1, price: 0,
+    holes: [
+    ]
+  },
+  {
+    id: "p9", plotnum: 9, shedpadtop: 752, shedpadleft: 392,
+    owner: -1, price: 0,
+    holes: [
+    ]
+  },
+  {
+    id: "p10", plotnum: 10, shedpadtop: 752, shedpadleft: 392,
+    owner: -1, price: 0,
+    holes: [
+    ]
+  },
+  {
+    id: "p11", plotnum: 11, shedpadtop: 752, shedpadleft: 392,
+    owner: -1, price: 0,
+    holes: [
+    ]
+  },
+  {
+    id: "p12", plotnum: 12, shedpadtop: 752, shedpadleft: 392,
+    owner: -1, price: 0,
+    holes: [
+    ]
+  },
+  {
+    id: "p13", plotnum: 13, shedpadtop: 752, shedpadleft: 392,
+    owner: -1, price: 0,
+    holes: [
+    ]
+  },
+  {
+    id: "p14", plotnum: 14, shedpadtop: 752, shedpadleft: 392,
+    owner: -1, price: 0,
+    holes: [
+    ]
+  },
+  {
+    id: "p15", plotnum: 15, shedpadtop: 752, shedpadleft: 392,
+    owner: -1, price: 0,
+    holes: [
+    ]
+  },
+  {
+    id: "p16", plotnum: 16, shedpadtop: 752, shedpadleft: 392,
+    owner: -1, price: 8000,
+    holes: [
+    { id: "16a", plot: 16, spoke: 39, ring: 3, top: 722, left: 539, drill: -1 },
+    { id: "16b", plot: 16, spoke: 39, ring: 4, top: 749, left: 528, drill: -1 },
+    { id: "16c", plot: 16, spoke: 39, ring: 6, top: 805, left: 505, drill: -1 },
+    { id: "16d", plot: 16, spoke: 40, ring: 3, top: 708, left: 512, drill: -1 },
+    { id: "16e", plot: 16, spoke: 40, ring: 5, top: 762, left: 481, drill: -1 },
+    { id: "16f", plot: 16, spoke: 41, ring: 4, top: 716, left: 468, drill: -1 },
+    { id: "16g", plot: 16, spoke: 41, ring: 6, top: 765, left: 430, drill: -1 },
+    { id: "16h", plot: 16, spoke: 42, ring: 5, top: 716, left: 420, drill: -1 }
+    ]
+  },
+  {
+    id: "p17", plotnum: 17, shedpadtop: 700, shedpadleft: 582,
+    owner: -1, price: 12000,
+    holes: [
+    { id: "17a", plot: 17, spoke: 33, ring: 3, top: 719, left: 717, drill: -1 },
+    { id: "17b", plot: 17, spoke: 34, ring: 2, top: 699, left: 678, drill: -1 },
+    { id: "17c", plot: 17, spoke: 34, ring: 3, top: 729, left: 687, drill: -1 },
+    { id: "17d", plot: 17, spoke: 34, ring: 4, top: 759, left: 697, drill: -1 },
+    { id: "17e", plot: 17, spoke: 35, ring: 3, top: 736, left: 659, drill: -1 },
+    { id: "17f", plot: 17, spoke: 35, ring: 5, top: 797, left: 666, drill: -1 },
+    { id: "17g", plot: 17, spoke: 36, ring: 2, top: 706, left: 627, drill: -1 },
+    { id: "17h", plot: 17, spoke: 36, ring: 4, top: 769, left: 629, drill: -1 },
+    { id: "17i", plot: 17, spoke: 36, ring: 5, top: 799, left: 629, drill: -1 },
+    { id: "17j", plot: 17, spoke: 37, ring: 3, top: 735, left: 599, drill: -1 },
+    { id: "17k", plot: 17, spoke: 37, ring: 4, top: 767, left: 595, drill: -1 },
+    { id: "17l", plot: 17, spoke: 37, ring: 5, top: 797, left: 590, drill: -1 }
+    ]
+  }
+]
