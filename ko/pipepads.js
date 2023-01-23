ff8fbc420b8b03cb209d68f9914bcd9ee2b8af2f
Robert Tivy <roberttivy@Laurens-MacBook-Pro-2.local>
2023-01-23 11:40:58 -0800

    Add pipe pad 13-14

--- ko/pipepads.js
+++ ko/pipepads.js
@@ -1,6 +1,8 @@
 var pipepads = [
     { id: "5-7", plotnumA: 5, plotnumB: 7, owner: -1, pipes: [],
       pipepadleft: 749, pipepadtop: 333, pipeorientation: "horizontal" },
+    { id: "13-14", plotnumA: 13, plotnumB: 14, owner: -1, pipes: [],
+      pipepadleft: 439, pipepadtop: 646, pipeorientation: "horizontal" },
     { id: "13-16", plotnumA: 13, plotnumB: 16, owner: -1, pipes: [],
       pipepadleft: 395, pipepadtop: 685, pipeorientation: "vertical" },
     { id: "16-17", plotnumA: 16, plotnumB: 17, owner: -1, pipes: [],
