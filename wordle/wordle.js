936c18a4411d22f808780c7b8aa09bb0ed256e87
Robert Tivy <roberttivy@Laurens-MacBook-Pro-2.local>
2025-02-20 10:48:40 -0800

    Add support for "in word but not here" letters

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -24,7 +24,7 @@ var d = document.createElement("DIV");
 d.style.width = "200px";
 d.style.height = "44px";
 d.style.position = "absolute";
-d.style.left = "415px";
+d.style.left = "405px";
 d.style.top = "50px";
 d.style.color = "white";
 d.style.backgroundColor = "#00000";
@@ -77,6 +77,70 @@ d.style.paddingLeft = "4px";
 d.innerHTML = "OBSCURE";
 document.body.appendChild(d);
 
+var t = document.createElement("TABLE");
+t.style.width = "300px";
+//t.style.height = "22px";
+t.style.position = "absolute";
+t.style.left = "650px";
+t.style.top = "20px";
+t.style.color = "white";
+t.style.backgroundColor = "#00000";
+t.style.font = "17px arial bold";
+t.style.textAlign = "center";
+//t.style.paddingTop = "4px";
+//t.style.paddingLeft = "4px";
+document.body.appendChild(t);
+
+var row = t.createTHead().insertRow(-1);
+row.insertCell(0).innerHTML = "";
+row.insertCell(1).innerHTML = "any";
+row.insertCell(2).innerHTML = "1st";
+row.insertCell(3).innerHTML = "2nd";
+row.insertCell(4).innerHTML = "3rd";
+row.insertCell(5).innerHTML = "4th";
+row.insertCell(6).innerHTML = "5th";
+
+var n_letter = [];
+var n_start = [];
+var n_2nd = [];
+var n_3rd = [];
+var n_4th = [];
+var n_end = [];
+var alpha = 'abcdefghijklmnopqrstuvwxyz';
+for (var l = 0; l < 26; l++) {
+    var a = alpha[l];
+    n_letter[a] = 0;
+    n_start[a] = 0;
+    n_2nd[a] = 0;
+    n_3rd[a] = 0;
+    n_4th[a] = 0;
+    n_end[a] = 0;
+}
+
+for (var word of Ma) {
+    n_start[word[0]]++;
+    n_2nd[word[1]]++;
+    n_3rd[word[2]]++;
+    n_4th[word[3]]++;
+    n_end[word[4]]++;
+
+    for (var l = 0; l < 5; l++) {
+        n_letter[word[l]]++;
+    }
+}
+
+for (l = 0; l < 26; l++) {
+    var a = alpha[l];
+    var row = t.insertRow(-1);
+    row.insertCell(0).innerHTML = a;
+    row.insertCell(1).innerHTML = n_letter[a];
+    row.insertCell(2).innerHTML = n_start[a];
+    row.insertCell(3).innerHTML = n_2nd[a];
+    row.insertCell(4).innerHTML = n_3rd[a];
+    row.insertCell(5).innerHTML = n_4th[a];
+    row.insertCell(6).innerHTML = n_end[a];
+}
+
 var canvas = document.createElement("canvas");
 canvas.style.width = "1000px";
 canvas.style.height = "666px";
@@ -84,10 +148,12 @@ canvas.style.backgroundColor = "#000000";
 document.body.insertBefore(canvas, document.body.childNodes[0]);
 
 window.addEventListener('keydown', keyDownEvent);
+window.addEventListener('keyup', keyUpEvent);
 
 var letterIndex = 0;
 var guess;
 var rowIndex = 110;
+var row = 0;
 
 function drawWord(word, type)
 {
@@ -121,12 +187,16 @@ function drawWord(word, type)
         d.style.transform = "scaleY(0.0)";
         d.style.position = "absolute";
         d.style.color = "white";
-        if (guess[i] == ".") {
-            d.style.backgroundColor = "#b59f3b";
+        if (guess[i] == '.' || guess[i] == ' ') {
+            d.style.backgroundColor = "#000000";  // black
+        }
+        else if (true) {
+            d.style.backgroundColor = "#538d4e";  // green
         }
         else {
-            d.style.backgroundColor = "#538d4e";
+            d.style.backgroundColor = "#b59f3b";  // yellow
         }
+d.style.backgroundColor = "#538d4e";  // green
         d.style.textAlign = "center";
         d.innerHTML = word[i];
 
@@ -139,7 +209,7 @@ function drawWord(word, type)
             { transform: "scaleY(1.0)" },
         ], {
             duration: 500,
-            delay: 100 * i,
+            delay: 100 * (row + i),
         });
         animation.onfinish = (event) => {
             event.target.element.style.transform = "scaleY(1.0)";
@@ -149,10 +219,13 @@ function drawWord(word, type)
         animation.play();
     }
 
+    row++;
     rowIndex += rowInc;
 
     return divs;
 }
+
+var shift = false;
 
 function keyDownEvent(e)
 {
@@ -160,11 +233,28 @@ function keyDownEvent(e)
 
     switch (e.code) {
       case "Backspace":
-        letterIndex--;
-        if (letterIndex < 0) {
-           letterIndex = 0;
+        if (shift) {
+            letterIndex = 0;
+            for (var i = 0; i < 5; i++) {
+                letters[i].innerHTML = "";
+            }
         }
-        letters[letterIndex].innerHTML = "";
+        else {
+            letterIndex--;
+            if (letterIndex < 0) {
+               letterIndex = 0;
+            }
+            letters[letterIndex].innerHTML = "";
+        }
+
+        while (solutionDivs.length > 0) {
+           var divs = solutionDivs.pop();
+           for (var i = 0; i < 5; i++) {
+               var d = divs[i];
+               d.doc.body.removeChild(d);
+           }
+        }
+
         break;
 
       case "Enter":
@@ -180,6 +270,7 @@ function keyDownEvent(e)
            }
         }
         rowIndex = 110;
+        row = 0;
 
         guess = "";
         for (var i = 0; i < 5; i++) {
@@ -193,6 +284,12 @@ function keyDownEvent(e)
      
         break;
 
+      case "ShiftLeft":
+      case "ShiftRight":
+        shift = true;
+
+        break;
+
       default:
         if (e.code.startsWith("Key") ||
             e.code == "Period" ||
@@ -202,23 +299,109 @@ function keyDownEvent(e)
             }
             var letter;
             if (e.code == "Period") {
-                letter = ".";
+                letter = '.';
             }
             else if (e.code == "Space") {
-                letter = " ";
+                letter = ' ';
             }
             else {
                 letter = e.code[3];
             }
-            letters[letterIndex++].innerHTML = letter
+
+            var div = letters[letterIndex++];
+            div.innerHTML = letter;
+            if (shift) {
+                div.placeType = "misplaced";
+                div.style.color = "#b59f3b";
+            }
+            else {
+                div.placeType = "exact";
+                div.style.color = "#538d4e";
+            }
+            if (letter == '.' || letter == ' ') {
+                div.placeType = "wildcard";
+                div.style.color = "white";
+            }
         }
+
         break;
     }
 }
 
+function keyUpEvent(e)
+{
+    console.log("key: " + e.code);
+
+    switch (e.code) {
+      case "ShiftLeft":
+      case "ShiftRight":
+        shift = false;
+
+        break;
+    }
+}
+
+function checkWord(word)
+{
+    var wordAttrs = [
+        { notLetters: [], checked: false },
+        { notLetters: [], checked: false },
+        { notLetters: [], checked: false },
+        { notLetters: [], checked: false },
+        { notLetters: [], checked: false },
+    ];
+    var misplacedLetters = [];
+
+    for (var l = 0; l < 5; l++) {
+        wordAttrs[l].checked = false;
+
+        var div = letters[l];
+        if (div.placeType == "exact") {
+            wordAttrs[l].checked = true;
+
+            if (word[l] != guess[l]) {
+                return false;
+            }
+        }
+        else if (div.placeType == "misplaced") {
+            wordAttrs[l].notLetters.push(guess[l]);
+            misplacedLetters.push({ letter: guess[l], found: false });
+        }
+    }
+    for (var l = 0; l < 5; l++) {
+        if (!wordAttrs[l].checked) {
+            var notLetters = wordAttrs[l].notLetters;
+            while (notLetters.length > 0) {
+                var notLetter = notLetters.pop();
+                if (word[l] == notLetter) {
+                    return false;
+                }
+            }
+        }
+    }
+    for (var l = 0; l < 5; l++) {
+        if (!wordAttrs[l].checked) {
+            for (var m = 0; m < misplacedLetters.length; m++) {
+                if (!wordAttrs[l].checked && !misplacedLetters[m].found &&
+                    word[l] == misplacedLetters[m].letter) {
+                    wordAttrs[l].checked = true;
+                    misplacedLetters[m].found = true;
+                }
+            }
+        }
+    }
+    while (misplacedLetters.length > 0) {
+        var m = misplacedLetters.pop();
+        if (m.found == false) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 function searchWordles(guess)
 {
-//    var guess = "tau.e";
 //    var guess = prompt("Enter guess (with . for unknown letter)");
 
     if (guess == undefined) {
@@ -230,90 +413,21 @@ function searchWordles(guess)
         return;
     }
 
-    var match;
-    var words = [];
-
-    var n_letter = [];
-    var n_start = [];
-    var n_2nd = [];
-    var n_3rd = [];
-    var n_4th = [];
-    var n_end = [];
-    var alpha = 'abcdefghijklmnopqrstuvwxyz';
-    for (var l = 0; l < 26; l++) {
-        var a = alpha[l];
-        n_letter[a] = 0;
-        n_start[a] = 0;
-        n_2nd[a] = 0;
-        n_3rd[a] = 0;
-        n_4th[a] = 0;
-        n_end[a] = 0;
-    }
-
     for (var word of Ma) {
-        n_start[word[0]]++;
-        n_2nd[word[1]]++;
-        n_3rd[word[2]]++;
-        n_4th[word[3]]++;
-        n_end[word[4]]++;
-
-        match = true;
-        for (var l = 0; l < 5; l++) {
-            n_letter[word[l]]++;
-
-            if (guess[l] == '.') {
-                continue;
-            }
-            if (guess[l] != word[l]) {
-                match = false;
-            }
-        }
-
-        if (match) {
-            words.push(word);
+        if (checkWord(word)) {
             solutionDivs.push(drawWord(word.toUpperCase(), "main"));
 
             console.log(word);
         }
     }
-    if (words.length == 0) {
-        console.log("No matching words");
-    }
 
     rowIndex = 10;
+    row = 0;
     for (var word of Oa) {
-        match = true;
-        for (var l = 0; l < 5; l++) {
-            if (guess[l] == '.') {
-                continue;
-            }
-            if (guess[l] != word[l]) {
-                match = false;
-                break;
-            }
-        }
-
-        if (match) {
-            words.push(word + "*");
+        if (checkWord(word)) {
             solutionDivs.push(drawWord(word.toUpperCase(), "obscure"));
 
             console.log(word + '*');
         }
     }
-
-    var out = guess + "\n";
-
-    for (var w of words) {
-        out += w + " ";
-    }
-
-    out += "\n";
-//    out = "";
-
-    for (var l = 0; l < 26; l++) {
-        var a = alpha[l];
-        out += a + "\t" + n_letter[a] + "\t" + (n_letter[a] < 1000 ? "\t" : "") + n_start[a] + "\t\t"  + n_2nd[a] + "\t\t" + n_3rd[a] + "\t\t"  + n_4th[a] + "\t\t"  + n_end[a] + "\n";
-    }
-
-//    alert(out);
 }
